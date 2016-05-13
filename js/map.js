cd934c39a2d28a6fff7b068c022f9ec53707810b
Daniel Otero Cortés <dotero@autentia.com>
2016-05-13 14:52:07 +0200

    Leaflet demo

---
+++ js/map.js
@@ -0,0 +1,16 @@
+
+var demoMap = L.map('map').setView([40.453191, -3.509236], 6);
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {
+    minZoom: 5,
+    maxZoom: 16,
+    attribution: osmAttrib
+});
+osm.addTo(demoMap);
+
+for (var i = 0; i < locations.length; i++) {
+    var marker = L.marker(locations[i])
+    marker.bindPopup('<p>Latitud:'+locations[i][0]+'</p><p>Longitud:'+locations[i][1]+'</p>');
+    marker.addTo(demoMap);
+}
